a0e7dd9510e44f659c298834bedae4b965adfea1
Fer <fernandoza@gmail.com>
2019-07-17 22:09:22 -0500

    feat: render poster in show page

--- app/src/main/java/com/fer_mendoza/fermdb/MovieDetailActivity.java
+++ app/src/main/java/com/fer_mendoza/fermdb/MovieDetailActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,13 +26,19 @@ public class MovieDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intentClicked = getIntent();
-        TextView title = findViewById(R.id. movie_title);
+        TextView title = findViewById(R.id.movie_title);
+        ImageView poster = findViewById(R.id.movie_poster);
         
         if(intentClicked.hasExtra("movieData")){
             String jsonArray = intentClicked.getStringExtra("movieData");
             try {
                 JSONObject movieData = new JSONObject(jsonArray);
-                title.setText(movieData.getString("title"));
+                String posterPath = "http://image.tmdb.org/t/p/w500" + movieData.getString("poster_path");
+
+//                toolbar.setTitle(movieData.getString("title"));
+//                toolbar.setTitle("FerMDB");
+//                toolbar.setSubtitle(movieData.getString("title"));
+                Picasso.get().load(posterPath).into(poster);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
